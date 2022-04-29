4a4bf79f1286f64f5c4a22d2e6200e09a11ee1b5
์Nuttawat <83624530+saturnooi@users.noreply.github.com>
2022-04-29 07:04:43 +0700

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,8 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: "static",
-  mode: 'spa',
+  mode: "universal",
+
   head: {
     title: "fe",
     htmlAttrs: {
